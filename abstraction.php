11f40702ec79bf2d14d550dc03cf84b0f329046b
Rawan Mortaja <33564662+rawan-mortja@users.noreply.github.com>
2022-12-14 23:51:01 +0200

    Update abstraction.php

--- abstraction.php
+++ abstraction.php
@@ -1,1 +1,36 @@
 
+<?php
+class Account
+{
+    protected $balance;
+
+    public function __construct($initialBalance)
+    {
+        $this->balance = $initialBalance;
+    }
+
+    public function deposit($amount)
+    {
+        $this->balance += $amount;
+    }
+
+    public function withdraw($amount)
+    {
+        if ($this->balance - $amount >= 0) {
+            $this->balance -= $amount;
+        } else {
+        }
+    }
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+}
+
+$account = new Account(100);
+
+$account->deposit(50);
+
+$account->withdraw(25);
+
+$balance = $account->getBalance();
