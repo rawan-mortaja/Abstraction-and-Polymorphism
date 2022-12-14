29c083ea9651a39adb45a24a87cf249feee6f9b2
Rawan Mortaja <33564662+rawan-mortja@users.noreply.github.com>
2022-12-14 23:46:46 +0200

    Update polymorphism.php

--- polymorphism.php
+++ polymorphism.php
@@ -1,1 +1,29 @@
-
+<?php
+   interface Machine {
+      public function calcTask();
+   }
+   class Circle implements Machine {
+      private $radius;
+      public function __construct($radius){
+         $this -> radius = $radius;
+      }
+      public function calcTask(){
+         return $this -> radius * $this -> radius * pi();
+      }
+   }
+   class Rectangle implements Machine {
+      private $width;
+      private $height;
+      public function __construct($width, $height){
+         $this -> width = $width;
+         $this -> height = $height;
+      }
+      public function calcTask(){
+         return $this -> width * $this -> height;
+      }
+   }
+   $mycirc = new Circle(3);
+   $myrect = new Rectangle(3,4);
+   echo $mycirc->calcTask();
+   echo $myrect->calcTask();
+?>
